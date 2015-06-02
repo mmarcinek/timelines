23df32e74e1dcc054b68ca573d65c173180d5834
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-02 16:43:07 -0400

    Array for each working..

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
-console.log('This is cool!');
+var data = $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function (data){
+data.timelines.forEach(function (timeline){
+  day = moment().format("M/DD/YY");
+  time = moment().format("hh:mm");
+  title = timeline.title;
+  text = timeline.content;
+  icon = timeline.icon;
+});
+});
 
 $('.container').html(template.example({
-  value: 'Hello, World!!'
+
 }));
+
+// $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function(data){
+//   data.timelines.map(function ())
+// });
+
--- js/templates.js
+++ js/templates.js
@@ -2,7 +2,7 @@ this["template"] = this["template"] || {};
 this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var helper;
 
-  return "<h1>\n  "
-    + this.escapeExpression(((helper = (helper = helpers.value || (depth0 != null ? depth0.value : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"value","hash":{},"data":data}) : helper)))
-    + "\n</h1>\n";
+  return "<h1>"
+    + this.escapeExpression(((helper = (helper = helpers.day || (depth0 != null ? depth0.day : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"day","hash":{},"data":data}) : helper)))
+    + "</h1>\n";
 },"useData":true});
