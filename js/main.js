9f6ad2d912871c6a31a5be07951346ca3c42eafd
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-02 18:07:28 -0400

    Creates an object of array data

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 var data = $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function (data){
 data.timelines.forEach(function (timeline){
-  day = moment().format("M/DD/YY");
-  time = moment().format("hh:mm");
-  title = timeline.title;
-  text = timeline.content;
-  icon = timeline.icon;
+  post = {
+  day: moment().format("M/DD/YY"),
+  time: moment().format("hh:mm"),
+  title: timeline.title,
+  text: timeline.content,
+  icon: timeline.icon
+};
 });
 });
 
