37e1338ae7e154cd4f0c8812ef70dec832ba9c5f
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-03 07:55:30 -0400

    Starts on responsive design, mobile view complete, large screen still buggy

--- js/templates.js
+++ js/templates.js
@@ -2,13 +2,13 @@ this["template"] = this["template"] || {};
 this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "  <section>\n    <div class=\"date\">\n      <ul>\n        <li class=\"day\">"
+  return "  <section>\n    <div class=\"side\">\n      <div class=\"date\">\n        <ul>\n          <li class=\"day\">"
     + alias3(((helper = (helper = helpers.day || (depth0 != null ? depth0.day : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"day","hash":{},"data":data}) : helper)))
-    + "</li>\n        <li class=\"time\">"
+    + "</li>\n          <li class=\"time\">"
     + alias3(((helper = (helper = helpers.time || (depth0 != null ? depth0.time : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"time","hash":{},"data":data}) : helper)))
-    + "</li>\n      </ul>\n    </div>\n    <div class=\"icon\">\n      <i class=\"fa "
+    + "</li>\n        </ul>\n      </div>\n      <div class=\"icon\">\n        <i class=\"fa "
     + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
-    + "\"></i>\n    </div>\n    <div class=\"content\">\n      <h3>"
+    + "\"></i>\n      </div>\n    </div>\n    <div class=\"content\">\n      <h3>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + "</h3>\n      <p>"
     + alias3(((helper = (helper = helpers.content || (depth0 != null ? depth0.content : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"content","hash":{},"data":data}) : helper)))
