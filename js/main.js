1e849b7104d2f73bc30fd7cd66c3d472c7d4ac29
Michael Marcinek <michaelmarcinek@gmail.com>
2015-06-02 21:41:56 -0400

    Working on handlebars.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,30 @@
+var post= {};
+
 var data = $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function (data){
 data.timelines.forEach(function (timeline){
   post = {
-  day: moment().format("M/DD/YY"),
-  time: moment().format("hh:mm"),
+  day: moment(timeline.timestamp).format("M/DD/YY"),
+  time: moment(timeline.timestamp).format("hh:mm"),
   title: timeline.title,
   text: timeline.content,
   icon: timeline.icon
 };
+console.log(post);
 });
 });
 
-$('.container').html(template.example({
+// var day, time, title, text, icon;
 
-}));
+$('.container').html(template.example(post));
+
 
 // $.getJSON('http://private-44f6d-timeline6.apiary-mock.com/timeline').done(function(data){
-//   data.timelines.map(function ())
+//     data.timelines.forEach(function (timeline){
+//     day = moment(timeline.timestamp).format("M/DD/YY");
+//     time = moment(timeline.timestamp).format("hh:mm");
+//     title = timeline.title;
+//     text = timeline.content;
+//     icon = timeline.icon;
+// });
 // });
 
--- js/templates.js
+++ js/templates.js
@@ -1,8 +1,20 @@
 this["template"] = this["template"] || {};
-this["template"]["example"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var helper;
+this["template"]["example"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "<h1>"
-    + this.escapeExpression(((helper = (helper = helpers.day || (depth0 != null ? depth0.day : depth0)) != null ? helper : helpers.helperMissing),(typeof helper === "function" ? helper.call(depth0,{"name":"day","hash":{},"data":data}) : helper)))
-    + "</h1>\n";
+  return "  <section>\n    <ul>\n      <li class=\"day\">"
+    + alias3(((helper = (helper = helpers.day || (depth0 != null ? depth0.day : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"day","hash":{},"data":data}) : helper)))
+    + "</li>\n      <li class=\"time\">"
+    + alias3(((helper = (helper = helpers.time || (depth0 != null ? depth0.time : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"time","hash":{},"data":data}) : helper)))
+    + "</li>\n    </ul>\n    <i class=\""
+    + alias3(((helper = (helper = helpers.icon || (depth0 != null ? depth0.icon : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"icon","hash":{},"data":data}) : helper)))
+    + "\"</i>\n    <div class=\"content\">\n      <h3>"
+    + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
+    + "</h3>\n      <p>"
+    + alias3(((helper = (helper = helpers.text || (depth0 != null ? depth0.text : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"text","hash":{},"data":data}) : helper)))
+    + "</p>\n    </div>\n  </section>\n";
+},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var stack1;
+
+  return ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.Post : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "");
 },"useData":true});
